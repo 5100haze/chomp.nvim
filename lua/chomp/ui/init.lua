c8ea80ca136a71064b7f8a1445ce75a72eafa8d8
haze <haze@otherside.sh>
2026-03-16 21:52:20 -0700

    just notes lol

---
+++ lua/chomp/ui/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.open = function() end
+
+-- vim.api.nvim_create_buf()
+-- vim.api.nvim_open_win()
+-- popup window opts
+-- buf opts (vim.api.nvim_buf_set_option)
+-- window opts (vim.api.nvim_win_set_option)
+-- nvim_create_namespace()
+
+-- drawing:
+-- nvim_buf_clear_namespace()
+-- nvim_buf_set_option() (set modifiable to true for just the update)
+-- nvim_buf_set_lines()
+
+return M
