a15a89a5388ea08029bc2b1c598c88f0df42a708
haze <haze@otherside.sh>
2026-03-17 06:40:12 -0700

    cleanup setup

--- lua/chomp/init.lua
+++ lua/chomp/init.lua
@@ -3,11 +3,8 @@ local M = {}
 ---@type Options
 local default_options = {}
 
-local config = {}
-
 M.setup = function(options)
   local opts = vim.tbl_deep_extend('force', default_options, options or {})
-  config = opts
 
   require 'chomp.api.commands'
 end
