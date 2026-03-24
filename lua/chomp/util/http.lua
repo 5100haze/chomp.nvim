f916b54800d637bd64f421448399e57a137fe9b6
haze <haze@otherside.sh>
2026-03-24 09:29:22 -0700

    add http functions

--- lua/chomp/util/http.lua
+++ lua/chomp/util/http.lua
@@ -3,24 +3,41 @@ local util = require 'chomp.util'
 
 local M = {}
 
+-- callbacks may or may not get called !!
+
 M.head = function(url, callback)
+  local parse_header = function(line)
+    local sep = string.find(line, ':', 1, true)
+    if not sep then return nil end
+    local key = string.sub(line, 1, sep - 1):lower()
+    local val = vim.trim(string.sub(line, sep + 1))
+    return key, val
+  end
   vim.system({ 'curl', '-L', '-s', '-I', util.url.normalize(url) }, { text = true }, function(obj)
     if obj.code ~= 0 then
       --notify
       return
     end
     local res = {}
-    --parse and make table of headers
+    local raw = obj.stdout
+    assert(type(raw) == 'string', 'nil output from curl')
+    local lines = vim.split(raw, '\n', { trimempty = true })
+    for i = 2, #lines do
+      local header, value = parse_header(lines[i])
+      if not header then return end
+      res[header] = value
+    end
+
     callback(res)
   end)
 end
 
 M.get = function(url, callback)
-  vim.system({ 'curl', '-L', '-s', '-I', util.url.normalize(url) }, { text = true }, function(obj)
+  vim.system({ 'curl', '-L', '-s', util.url.normalize(url) }, { text = true }, function(obj)
     if obj.code ~= 0 then return end
-    local res = {}
-    --send string blob to callback
-    callback(res)
+    local raw = obj.stdout
+    assert(type(raw) == 'string', 'nil output from curl')
+    callback(raw)
   end)
 end
 
