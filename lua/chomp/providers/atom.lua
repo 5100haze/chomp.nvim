be0f3abea11a69a8f937f84cb7e9c65c42d565a0
haze <haze@otherside.sh>
2026-03-29 17:39:50 -0700

    add atom parser

--- lua/chomp/providers/atom.lua
+++ lua/chomp/providers/atom.lua
@@ -1,15 +1,78 @@
+local xml = require 'chomp.util.xml'
+
 local Base = {}
 Base.__index = Base
 
+---@class Atom10
+---@field _title string
+---@field _updated string
+---@field _els table[]
+---@field _entries_from integer
 local Atom10 = setmetatable({}, Base)
 Atom10.__index = Atom10
 
-Atom10.title = function(self) end
+---@type table<string, string> atom name => post field name
+local POST_FIELDS = {
+  title = 'title',
+  link = 'url',
+  id = 'guid',
+  summary = 'summary',
+  published = 'published_at',
+  updated = 'updated_at',
+}
 
-Atom10.last_updated = function(self) end
+local find_feed_el = function(doc)
+  for _, k in ipairs(doc.kids) do
+    if k.name == 'feed' then return k end
+  end
+end
 
-Atom10.posts = function(self) end
+---@param doc table
+---@return Atom10
+Atom10.new = function(doc)
+  local self = setmetatable({ _doc = doc }, Atom10)
+
+  local feedel = find_feed_el(doc)
+  if not feedel then error 'no <feed> element' end
+
+  local channel_els = feedel.el
+  local i = 1
+  while channel_els[i] and channel_els[i].name ~= 'entry' do
+    i = i + 1
+  end
+
+  local meta = xml.extract({ 'title', 'updated' }, { table.unpack(channel_els, 1, i - 1) })
+  self._title = meta.title
+  self._updated = meta.updated
+  self._entries_from = i
+  self._els = channel_els
+
+  return self
+end
+
+---@return string?
+Atom10.get_title = function(self) return self._title end
+
+---@return string?
+Atom10.get_updated = function(self) return self._updated end
+
+---@return fun(): Post?
+Atom10.posts = function(self)
+  local want = vim.tbl_keys(POST_FIELDS)
+  local i = self._entries_from - 1
+  local els = self._els
+
+  return function()
+    i = i + 1
+    while els[i] and els[i].name ~= 'entry' do
+      i = i + 1
+    end
+    if not els[i] then return nil end
+    local raw = xml.extract(want, els[i].el)
+    return xml.map_fields(raw, POST_FIELDS)
+  end
+end
 
 return {
-  [1.0] = Atom10,
+  ['1.0'] = Atom10,
 }
