81d61c3fae2b082c0d881cfe1bf6dbdc692748ce
haze <haze@otherside.sh>
2026-04-25 16:46:43 -0700

    include headers in http.get() response

--- lua/chomp/util/http.lua
+++ lua/chomp/util/http.lua
@@ -2,8 +2,8 @@ local url = require 'chomp.util.url'
 
 local M = {}
 
-M.head = function(_url, callback)
-  local parse_header = function(line)
+local parse_headers = function(raw)
+  local parse_single = function(line)
     local sep = string.find(line, ':', 1, true)
     if not sep then return nil end
     local key = string.sub(line, 1, sep - 1):lower()
@@ -11,29 +11,37 @@ M.head = function(_url, callback)
     return key, val
   end
 
+  local res = {}
+  local lines = vim.split(raw, '\n', { trimempty = true })
+  for i = 2, #lines do
+    local header, value = parse_single(lines[i])
+    if not header then break end
+    res[string.lower(header)] = value
+  end
+
+  return res
+end
+
+M.head = function(_url, callback)
   vim.system({ 'curl', '-L', '-s', '-I', url.normalize(_url) }, { text = true }, function(obj)
     if obj.code ~= 0 then
       callback(nil, 'curl exited with code ' .. obj.code .. ': ' .. (obj.stderr or ''))
       return
     end
     if type(obj.stdout) ~= 'string' then callback(nil, 'no output from curl') end
-    local res = {}
-    local lines = vim.split(obj.stdout, '\n', { trimempty = true })
-    for i = 2, #lines do
-      local header, value = parse_header(lines[i])
-      if not header then break end
-      res[header] = value
-    end
 
-    callback(res, nil)
+    callback(parse_headers(obj.stdout), nil)
   end)
 end
 
 M.get = function(_url, callback)
-  vim.system({ 'curl', '-L', '-s', url.normalize(_url) }, { text = true }, function(obj)
+  vim.system({ 'curl', '-L', '-s', '-D', '%', url.normalize(_url) }, { text = true }, function(obj)
     if obj.code ~= 0 then callback(nil, 'curl exited with code ' .. obj.code .. ': ' .. (obj.stderr or '')) end
     if type(obj.stdout) ~= 'string' then callback(nil, 'no output from curl') end
-    callback(obj.stdout, nil)
+    callback({
+      xml = obj.stdout,
+      headers = parse_headers(obj.stderr),
+    }, nil)
   end)
 end
 
